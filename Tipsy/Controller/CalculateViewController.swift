66207bc38db40f0527f0e22bb5831660f13d0549
michaeltaranik <taranikmichael32@gmail.com>
2024-07-05 13:34:37 +0200

    design initialization

---
+++ Tipsy/Controller/CalculateViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    
+    @IBOutlet var enteredText: UITextField!
+    @IBOutlet var zeroTipButton: UIButton!
+    @IBOutlet var tenTipButton: UIButton!
+    @IBOutlet var twentyTipButton: UIButton!
+    @IBOutlet var personsToSplit: UILabel!
+    @IBOutlet var togglePersons: UIStepper!
+    
+    var currentTipButton: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentTipButton = tenTipButton
+    }
+    
+    
+    
+    @IBAction func tipChosen(_ sender: UIButton) {
+        currentTipButton?.isSelected = false
+        sender.isSelected = true
+        currentTipButton = sender
+    
+    }
+    
+    
+    @IBAction func togglePersonsIsPressed(_ sender: UIStepper) {
+        personsToSplit.text = String(Int(togglePersons.value)
+)    }
+    
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
